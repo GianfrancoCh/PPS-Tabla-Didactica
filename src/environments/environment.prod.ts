c29f7eb7c98c498710f5b6bbf30af7fffd6b23a5
GianfrancoCh <98627004+GianfrancoCh@users.noreply.github.com>
2025-04-28 00:07:10 -0300

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://yiilcrtfurocrzijkuwl.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpaWxjcnRmdXJvY3J6aWprdXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NjUzMTMsImV4cCI6MjA1OTU0MTMxM30.Jmp6fyZumwJotBdHbLv8wahKLo6KcJQNeImn0Ov5XPs',
-};
